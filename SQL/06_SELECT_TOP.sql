ab230d55fd2bbeb03e5ca85506133da173790fa0
Kiên Lê <100947959+trungkien2543@users.noreply.github.com>
2026-01-31 16:05:08 +0700

    feat: Bài 5, 6, 7

---
+++ SQL/06_SELECT_TOP.sql
@@ -0,0 +1,30 @@
+﻿-- VIẾT CÂU LỆNH LẤY RA 5 DÒNG ĐẦU TIÊN TRONG BẢNG CUSTOMERS
+SELECT TOP 5 *
+FROM Customers
+
+-- VIẾT CÂU LỆNH LẤY RA 30% NHÂN VIÊN CỦA CÔNG TY
+SELECT TOP 30 PERCENT *
+FROM Employees
+
+-- VIẾT CÂU LỆNH LẤY RA CÁC ĐƠN HÀNG VỚI QUY ĐỊNH LÀ MÃ KHÁCH HÀNG KHÔNG ĐƯỢC TRÙNG LẶP, CHỈ LẤY 5 DÒNG DỮ LIỆU ĐẦU TIÊN
+SELECT DISTINCT TOP 5 CustomerID
+FROM Customers
+
+-- BÀI TẬP
+-- VIẾT CÂU LỆNH LẤY RA CÁC SẢN PHẨM CÓ MÃ THỂ LOẠI KHÔNG BỊ TRÙNG LẶP, VÀ CHỈ LẤY RA 3 DÒNG ĐẦU TIÊN
+SELECT DISTINCT TOP 3 CategoryID 
+FROM Products
+
+-- Truy vấn 20% không trùng lặp ShipName và ShipAddress trong bảng Orders.
+-- SẼ LẤY SẮP XẾP THEO CỘT ĐƯỢC XẾP TRƯỚC
+SELECT DISTINCT TOP 20 PERCENT ShipAddress, ShipName 
+FROM Orders
+
+SELECT DISTINCT TOP 20 PERCENT ShipName, ShipAddress
+FROM Orders
+
+-- Truy vấn 120 dòng đầu trong bảng Orders.
+SELECT TOP 120 *
+FROM Orders
+
+
---
+++ SQL/07_ALIAS.sql
@@ -0,0 +1,17 @@
+﻿-- VIẾT CÂU LỆNH LẤY COMPANYNAME VÀ ĐẶT TÊN THAY THẾ LÀ CÔNG TY; POSTALCODE VÀ ĐẶT TÊN THAY THẾ LÀ MÃ BƯU ĐIỆN
+SELECT CompanyName AS "TÊN CÔNG TY", PostalCode AS "MÃ BƯU ĐIỆN", City AS "THÀNH PHỐ"
+FROM Customers
+
+-- VIẾT CÂU LỆNH LẤY RA LASTNAME VÀ ĐẶT TÊN THAY THẾ LÀ HỌ, FIRSTNAME VÀ ĐẶT TÊN THAY THẾ LÀ TÊN
+SELECT LastName AS "HỌ", FirstName AS "TÊN" 
+FROM Employees
+
+-- VIẾT CÂU LỆNH LẤY RA 15 DÒNG ĐẦU TIÊN TẤT CẢ CÁC CỘT TRONG BẢNG ORDERS, ĐẶT TÊN THAY THẾ CHO BẢNG ORDERS LÀ O
+SELECT TOP 15 *
+FROM Orders AS O
+
+-- BÀI TẬP
+-- VIẾT CÂU LỆNH LẤY RA 5 DÒNG VỚI CÁC CỘT VÀ ĐẶT TÊN THAY THẾ; ĐẶT TÊN THAY THẾ CHO BẢNG PRODUCTS LÀ P, SỬ DỤNG TÊN THAY THẾ ĐỂ TRUY VẤN
+SELECT TOP 5 P.ProductName AS "TÊN SẢN PHẨM", P.SupplierID AS "MÃ NHÀ CUNG CẤP", P.CategoryID AS "MÃ THỂ LOẠI"
+FROM Products AS P
+
