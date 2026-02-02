584900d183542c67bce2a9a4873ed93e00587404
Kiên Lê <100947959+trungkien2543@users.noreply.github.com>
2026-02-02 10:45:43 +0700

    feat: bài học 8, 9

---
+++ SQL/08_MAX_MIN.sql
@@ -0,0 +1,26 @@
+﻿-- LƯU Ý: CHỈ CÓ THỂ LẤY RA 1 CỘT KHI SỬ DỤNG MIN CHO 1 LẦN SELECT
+
+-- VIẾT CÂU LỆNH SQL TÌM GIÁ TRỊ THẤP NHẤT CỦA CÁC SẢN PHẨM TRONG BẢNG PRODUCTS
+SELECT MIN(UnitPrice) AS "MIN PRICE"
+FROM Products
+
+-- VIẾT CÂU LỆNH LẤY RA NGÀY ĐẶT HÀNG GẦN ĐÂY NHẤT TỪ BẢNG ORDERS
+SELECT MAX(OrderDate) AS "NGÀY ĐẶT HÀNG GẦN ĐÂY NHẤT"
+FROM Orders
+
+-- VIẾT CÂU LỆNH LẤY RA SẢN PHẨM (LẤY MÃ VÀ TÊN) CÓ SỐ LƯỢNG HÀNG TỒN KHO LỚN NHẤT
+SELECT ProductID, ProductName
+FROM Products
+WHERE UnitsInStock = (SELECT MAX(UnitsInStock) FROM Products)
+-- WHERE UnitsInStock = (SELECT TOP 1 UnitsInStock FROM Products ORDER BY UnitsInStock DESC) -> KHÔNG TỐI ƯU VÌ PHẢI SẮP XẾP TOÀN BẢNG
+
+-- BÀI TẬP: 
+-- 1. TÌM NGƯỜI CÓ TUỔI ĐỜI LỚN NHẤT CÔNG TY
+SELECT MIN(BirthDate) AS "NGÀY SINH LỚN NHẤT"
+FROM Employees
+
+-- 2. TÌM SỐ LỰƠNG HÀNG ĐƯỢC ĐẶT ÍT NHẤT TRONG BẢNG PRODUCT
+SELECT MIN(UnitsOnOrder) AS "SỐ LƯỢNG HÀNG ĐƯỢC ĐẶT ÍT NHẤT"
+FROM Products
+
+
---
+++ SQL/09_COUNT_SUM_AVG.sql
@@ -0,0 +1,48 @@
+﻿-- LƯU Ý: SUM, AVG SẼ BỎ QUA GIÁ TRỊ NULL
+
+-- ĐẾM SỐ LƯỢNG KHÁCH HÀNG CÓ TRONG BẢNG CUSTOMERS
+SELECT COUNT(*) AS "SỐ LƯỢNG KHÁCH HÀNG" 
+FROM Customers
+
+SELECT COUNT(CustomerID) AS "SỐ LƯỢNG KHÁCH HÀNG" 
+FROM Customers
+
+-- TÍNH TỔNG SỐ TIỀN VẤN CHUYỂN CỦA TẤT CẢ CÁC ĐƠN HÀNG
+SELECT SUM(Freight) AS "TỔNG SỐ TIỀN VẬN CHUYỂN"
+FROM Orders
+
+SELECT AVG(Freight) AS "TỔNG SỐ TIỀN VẬN CHUYỂN"
+FROM Orders
+
+-- TÍNH TRUNG BÌNH SỐ LƯỢNG ĐẶT HÀNG CỦA TẤT CẢ CÁC SẢN PHẨM TRONG BẢNG ORDER DETAILS
+SELECT AVG(Quantity) AS "SỐ LƯỢNG ĐẶT HÀNG TRUNG BÌNH"
+FROM [Order Details]
+
+-- ĐẾM SỐ LƯỢNG, TÍNH TỔNG SỐ LƯỢNG HÀNG TRONG KHO VÀ TRUNG BÌNH GIÁ CỦA CÁC SẢN PHẨM CÓ TRONG BẢNG PRODUCT
+SELECT 
+	COUNT(*) AS "SỐ LƯỢNG SẢN PHẨM", 
+	SUM(UnitsInStock) AS "TỔNG SỐ LƯỢNG HÀNG TỒN KHO",
+	AVG(UnitPrice) AS "TRUNG BÌNH GIÁ SẢN PHẨM"
+FROM Products
+
+-- BÀI TẬP
+-- 1. HÃY ĐẾM SỐ LƯỢNG ĐƠN HÀNG TỪ BẢNG ORDER BẰNG 2 CÁCH:
+-- CÁCH 1: DÙNG DẤU *
+SELECT COUNT(*) AS "SỐ LƯỢNG ĐƠN HÀNG"
+FROM Orders
+
+-- CÁCH 2: DÙNG MÃ ĐƠN HÀNG
+SELECT COUNT(OrderID) AS "SỐ LƯỢNG ĐƠN HÀNG"
+FROM Orders
+
+-- 2. TÍNH GIÁ TRỊ TRUNG BÌNH GIÁ  VÀ TỔNG SỐ LƯỢNG Ở BẢNG ORDER DETAILS
+SELECT AVG(UnitPrice) AS "GIÁ TRUNG BÌNH", SUM(Quantity) AS "TỔNG SỐ LƯỢNG"
+FROM [Order Details]
+
+-- 3. Tính số lượng CategoryID của bảng Categories
+SELECT COUNT(CategoryID) AS "SỐ LƯỢNG DANH MỤC"
+FROM Categories
+
+-- 4. Tính tổng số ReorderLevel bảng Products
+SELECT SUM(ReorderLevel) AS "MỨC ĐỘ ĐƯỢC MUA LẠI"
+FROM Products
