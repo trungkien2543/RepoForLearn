09294d84ef5d39f0f3e8c669d64e011ae34a9b09
Kiên Lê <100947959+trungkien2543@users.noreply.github.com>
2026-02-03 20:51:56 +0700

    feat: Bài 10, 11

---
+++ SQL/10_ORDER_BY.sql
@@ -0,0 +1,41 @@
+﻿-- HÃY LIỆT KÊ TẤT CẢ CÁC NHÀ CUNG CẤP THEO THỨ TỰ TÊN ĐƠN VỊ (CompanyName) TỪ A-Z
+SELECT *
+FROM Suppliers
+ORDER BY CompanyName ASC
+
+-- HÃY LIỆT KÊ TẤT CẢ CÁC SẢN PHẨM THEO THỨ TỰ GIÁ GIẢM DẦN
+SELECT *
+FROM Products
+ORDER BY UnitPrice DESC
+
+-- HÃY LIỆT KÊ TẤT CẢ CÁC NHÂN VIÊN THEO THỨ TỰ HỌ VÀ TÊN ĐỆM A-Z
+-- CÓ THỂ SẮP XẾP NHIỀU CỘT -> SQL SẼ SẮP XẾP THEO THỨ TỰ LẦN LƯỢT CỦA TỪNG CỘT
+SELECT *
+FROM Employees
+ORDER BY LastName, FirstName
+
+-- HÃY LẤY RA MỘT SẢN PHẨM CÓ SỐ LƯỢNG BÁN CAO NHẤT TỪ BẢNG ORDER DETAIL, KHÔNG DÙNG MAX
+SELECT TOP 1 *
+FROM [Order Details]
+ORDER BY Quantity DESC
+
+-- BÀI TẬP
+-- HÃY LIỆT KÊ DANH SÁCH CÁC ĐƠN ĐẶT HÀNG TRONG BẢNG ORDER THEO THỨ TỰ GIẢM DẦN CỦA NGÀY ĐẶT 
+SELECT OrderID
+FROM Orders
+ORDER BY OrderDate DESC
+
+-- HÃY LIỆT KÊ TÊN, ĐƠN GIÁ, SỐ LƯỢNG TRONG KHO CỦA TẤT CẢ CÁC SẢN PHẨM TRONG BẢNG PRODUCTS, THEO THỨ TỰ GIẢM DẦN CỦA UnitInStock
+SELECT ProductName, UnitPrice, UnitsInStock
+FROM Products
+ORDER BY UnitsInStock DESC
+
+-- lấy ra tên quốc gia của 4 khách hàng đầu tiên và tại các đất nước khác nhau và sx tên quốc gia theo thứ tự từ z-a
+SELECT DISTINCT TOP 4 Country
+FROM Customers
+ORDER BY Country DESC
+
+-- Hãy lấy ra tên nhân viên, ngày sinh, địa chỉ và sắp xếp nhân viên theo tuổi giảm dần
+SELECT LastName + FirstName AS "HỌ VÀ TÊN", BirthDate, Address
+FROM Employees
+ORDER BY BirthDate
---
+++ SQL/11_OPERATION.sql
@@ -0,0 +1,30 @@
+﻿-- TÍNH SỐ LƯỢNG SẢN PHẨM CÒN LẠI TRONG KHO SAU KHI BÁN HẾT CÁC SẢN PHẨM ĐÃ ĐƯỢC ĐẶT HÀNG 
+-- StockRemaining = UnitsInStock - UnitsOnOrder
+SELECT UnitsInStock, UnitsOnOrder, UnitsInStock - UnitsOnOrder AS "StockRemaining"
+FROM Products
+
+-- TÍNH GIÁ TRỊ ĐƠN HÀNG CHI TIẾT CHO TẤT CẢ CÁC SẢN PHẨM TRONG BẢNG ORDER DETAILS
+-- OrderDetailValue = UnitPrice x Quantity
+SELECT *, UnitPrice * Quantity - Discount AS "OrderDetailValue"
+FROM [Order Details]
+
+-- TÍNH TỶ LỆ GIÁ VẬN CHUYỂN ĐƠN ĐẶT HÀNG (Freight) TRUNG BÌNH CỦA CÁC ĐƠN HÀNG TRONG BẢNG ORDERS SO VỚI GIÁ TRỊ VẬN CHUYỂN CỦA ĐƠN HÀNG LỚN NHẤT
+SELECT AVG(Freight) /  MAX (Freight) AS "FreightRatio"
+FROM Orders
+
+
+-- BÀI TẬP
+-- HÃY LIỆT KÊ DANH SÁCH CÁC SẢN PHẨM, VÀ GIÁ CỦA TỪNG SẢN PHẨM SẼ ĐƯỢC GIẢM ĐI 10%
+-- CÁCH 1: DÙNG PHÉP NHÂN + PHÉP CHIA
+SELECT *, UnitPrice - UnitPrice * 10 / 100 AS "DISCOUNT 10%"
+FROM Products
+
+SELECT *, UnitPrice * 90 / 100 AS "DISCOUNT 10%"
+FROM Products
+
+-- CÁCH 2: CHỈ ĐƯỢC DÙNG PHÉP NHÂN
+SELECT *, UnitPrice - UnitPrice * 0.1 AS "DISCOUNT 10%"
+FROM Products
+
+SELECT *, UnitPrice * 0.9 AS "DISCOUNT 10%"
+FROM Products
