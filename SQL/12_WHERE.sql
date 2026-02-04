fedcc6cbe758a1a19ba33ece53d601f2d54c774f
Kiên Lê <100947959+trungkien2543@users.noreply.github.com>
2026-02-04 10:08:38 +0700

    feat: học bài 12, 13

---
+++ SQL/12_WHERE.sql
@@ -0,0 +1,62 @@
+﻿-- SQL KHÔNG PHÂN BIỆT HOA VỚI THƯỜNG
+
+-- HÃY LIỆT KÊ TẤT CẢ CÁC NHÂN VIÊN ĐẾN TỪ THÀNH PHỐ LONDON
+SELECT *
+FROM Employees
+WHERE City = 'LONDON'
+
+-- HÃY LIỆT KÊ TẤT CẢ CÁC NHÂN VIÊN ĐẾN TỪ THÀNH PHỐ LONDON
+-- SẮP XẾP KẾT QUẢ THEO LAST NAME A->Z
+SELECT *
+FROM Employees
+WHERE City = 'LONDON'
+ORDER BY LastName 
+
+-- HÃY LIỆT KÊ TẤT CẢ CÁC ĐƠN HÀNG BỊ GIAO MUỘN, BIẾT RẰNG NGÀY CẦN PHẢI GIAO HÀNG LÀ RequiredDate, NGÀY GIAO HÀNG THỰC TẾ LÀ ShippedDate
+SELECT OrderID, RequiredDate, ShippedDate
+FROM Orders
+WHERE RequiredDate < ShippedDate
+ORDER BY RequiredDate, ShippedDate
+
+-- LẤY RA TẤT CẢ CÁC ĐƠN HÀNG CHI TIẾT ĐƯỢC GIẢM GIÁ NHIỀU HƠN 10%
+SELECT *
+FROM [Order Details]
+WHERE Discount > 0.1
+
+-- LẤY RA TẤT CẢ CÁC ĐƠN HÀNG CHI TIẾT ĐƯỢC GIẢM GIÁ TỪ 10% ĐẾN 20%
+SELECT *
+FROM [Order Details]
+WHERE Discount > 0.1 AND Discount < 0.2
+
+-- BÀI TẬP
+-- HÃY LIỆT KÊ TẤT CẢ CÁC ĐƠN HÀNG ĐƯỢC GỬI ĐẾN QUỐC GIA LÀ FRANCE
+SELECT *
+FROM Orders
+WHERE ShipCountry = 'FRANCE'
+
+-- HÃY LIỆT KÊ CÁC SẢN PHẨM CÓ SỐ LƯỢNG HÀNG TRONG KHO LỚN HƠN 20
+SELECT *
+FROM Products
+WHERE UnitsInStock > 20
+ORDER BY UnitsInStock
+
+-- hãy liệt kê các sản phẩm có giá bán lớn hơn hoặc bằng 2 lần sản phẩm có giá bán nhỏ nhất. sau đó sắp xếp chúng theo thứ tự từ bé đến lớn.
+SELECT *
+FROM Products
+WHERE UnitPrice >= 2 * (SELECT MIN(UnitPrice) FROM Products)
+ORDER BY UnitPrice
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SQL/13_AND_OR_NOT.sql
@@ -0,0 +1,47 @@
+﻿--  HÃY LIỆT KÊ TẤT CẢ CÁC SẢN PHẨM CÓ SỐ LƯỢNG TRONG KHO THUỘC KHOẢNG NHỎ HƠN 50 HOẶC LỚN HƠN 100
+SELECT *
+FROM Products
+WHERE UnitsInStock < 50 OR UnitsInStock > 100
+ORDER BY UnitsInStock
+
+-- HÃY LIỆT KÊ CÁC ĐƠN HÀNG ĐƯỢC GIAO ĐẾN BRAZIL, ĐÃ BỊ GIAO MUỘN, BIẾT RẰNG NGÀY CẦN PHẢI GIAO LÀ REQUIRED DATE, NGÀY GIAO HÀNG THỰC TẾ LÀ SHIPPED DATE
+SELECT *
+FROM Orders
+WHERE ShipCountry = 'BRAZIL' AND RequiredDate < ShippedDate
+
+-- HÃY LẤY RA TẤT CẢ CÁC SẢN PHẨM CÓ GIÁ DƯỚI 100$ VÀ MÃ THỂ LOẠI KHÁC 1
+-- LƯU Ý: DÙNG NOT
+SELECT *
+FROM Products
+WHERE  NOT (UnitPrice >= 100 OR CategoryID = 1)
+
+-- BÀI TẬP
+-- HÃY LIỆT KÊ TẤT CẢ CÁC ĐƠN HÀNG CÓ GIÁ VẬN CHUYỂN TRONG KHOẢNG TỪ [50,100] ĐÔ LA
+SELECT *
+FROM Orders
+WHERE Freight >=50 AND Freight <=100
+ORDER BY Freight
+
+SELECT *
+FROM Orders
+WHERE NOT (Freight < 50 OR Freight > 100)
+ORDER BY Freight
+
+-- HÃY LIỆT KÊ SẢN PHẨM CÓ SỐ LƯỢNG HÀNG TRONG KHO LỚN HƠN 20 VÀ SỐ LƯỢNG HÀNG TRONG ĐƠN HÀNG NHỎ HƠN 20
+SELECT *
+FROM Products
+WHERE UnitsInStock > 20 AND UnitsOnOrder < 20
+
+-- Lấy ra top 3 nhân viên vào công ty sớm nhất & đến từ USA
+SELECT TOP 3 *
+FROM Employees
+WHERE Country = 'USA'
+ORDER BY HireDate
+
+-- Lấy ra các sản phẩm có giá > 20 và số lượng > 25 và đang được giảm giá ; sắp theo thứ tự giá từ nhỏ đến lớn
+SELECT *
+FROM Products
+WHERE UnitPrice > 20 AND UnitsInStock > 25 AND Discontinued = 1
+ORDER BY UnitPrice ASC
+
+
