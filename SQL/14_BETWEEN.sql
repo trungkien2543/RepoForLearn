503c15f8fe7aa3ae7d0bda35fdc4f26d3139003b
Kiên Lê <100947959+trungkien2543@users.noreply.github.com>
2026-02-05 10:01:00 +0700

    feat: bài 14,15

---
+++ SQL/14_BETWEEN.sql
@@ -0,0 +1,50 @@
+﻿-- LẤY DANH SÁCH CÁC SẢN PHẨM CÓ GIÁ BÁN TRONG KHOẢNG TỪ 10 ĐẾN 20 ĐÔ LA
+SELECT *
+FROM Products
+WHERE UnitPrice BETWEEN 10 AND 20
+
+-- LẤY DANH SÁCH CÁC ĐƠN ĐẶT HÀNG ĐƯỢC ĐẶT TỪ NGÀY 1996-07-01 ĐẾN 1996-07-31
+SELECT *
+FROM Orders
+WHERE OrderDate BETWEEN '1996-07-01' AND '1996-07-31'
+
+-- TÍNH TỔNG SỐ TIỀN VẬN CHUYỂN CỦA CÁC ĐƠN ĐẶT HÀNG ĐƯỢC ĐẶT TRONG KHOẢNG THỜI GIAN TỪ 1996-07-01 ĐẾN NGÀY 1996-07-31
+SELECT SUM(Freight) AS "TỔNG SỐ TIỀN VẬN CHUYỂN"
+FROM Orders
+WHERE OrderDate BETWEEN '1996-07-01' AND '1996-07-31'
+
+
+-- BÀI TẬP
+-- LẤY DANH SÁCH CÁC ĐƠN ĐẶT HÀNG CÓ NGÀY ĐẶT HÀNG TRONG KHOẢNG TỪ 1/1/1997 ĐẾN 31/12/1997 VÀ ĐƯỢC VẬN CHUYỂN BẰNG TÀU THỦY (SHIPVIA = 3)
+SELECT *
+FROM Orders
+WHERE OrderDate BETWEEN '1997-01-01' AND '1997-12-31' AND ShipVia = 3
+
+--Thử thách 1: Viết câu lệnh SQL liệt kê họ và tên nhân viên,
+-- chỉ sống ở London,
+--có ngày tháng năm sinh từ đến 1/1/1952 đến 31/12/1963 
+--hoặc ngày tuyển từ 1/2/1992 đến 31/12/1993
+-- chỉ lấy 3 nhân viên đầu tiên sắp xếp theo [LastName] A-Z.
+SELECT TOP 3 *
+FROM Employees
+WHERE City = 'LONDON' AND (BirthDate BETWEEN '1952-01-01' AND '1963-12-31' OR HireDate BETWEEN '1992-02-01' AND '1993-12-31')
+ORDER BY LastName
+
+--Thử thách 2: Viết câu lệnh SQL liệt kê các đơn hàng, 
+-- [OrderDate] từ 01-07-1996 đến 01-09-1996
+-- [OrderID] từ 10000 đến 10250
+--Sắp xếp theo [ShipName] Z-A.
+
+SELECT *
+FROM Orders
+WHERE OrderDate BETWEEN '1996-07-01' AND '1996-09-01' AND OrderID BETWEEN 10000 AND 10250
+ORDER BY ShipName DESC
+
+
+
+
+
+
+
+
+
---
+++ SQL/15_LIKE.sql
@@ -0,0 +1,57 @@
+﻿-- DẤU % ĐỂ ĐẠI DIỆN CHO KHÔNG, MỘT HOẶC NHIỀU KÝ TỰ
+-- DẤU _ ĐỂ ĐẠI DIỆN CHO MỘT KÝ TỰ ĐƠN
+
+-- HÃY LỌC RA TẤT CẢ CÁC KHÁCH HÀNG ĐẾN TỪ CÁC QUỐC GIA BẮT ĐẦU BẰNG CHỮ A 
+SELECT *
+FROM Customers
+WHERE Country LIKE 'A%'
+
+-- LẤY DANH SÁCH CÁC ĐƠN ĐẶT HÀNG ĐƯỢC GỬI ĐẾN TỪ THÀNH PHỐ CÓ CHỨA CHỮ a
+SELECT *
+FROM Orders
+WHERE ShipCity LIKE '%a%'
+
+-- LỌC RA TẤT CẢ CÁC ĐƠN HÀNG VỚI ĐIỀU KIỆN:
+-- SHIPCOUNTRY LIKE 'U_'
+-- SHIPCOUNTRY LIKE 'U%'
+SELECT *
+FROM Orders
+WHERE ShipCountry LIKE 'U_'
+
+SELECT *
+FROM Orders
+WHERE ShipCountry LIKE 'U%'
+
+-- BÀI TẬP
+-- HÃY LẤY RA TẤT CẢ CÁC NHÀ CUNG CẤP HÀNG CÓ CHỮ 'B' TRONG TÊN CỦA CÔNG TY
+SELECT *
+FROM Suppliers
+WHERE CompanyName LIKE '%B%'
+
+
+--Challenge 1: Write an SQL statement to list employees fullname with the letter "e" in the last name 
+--and born from 1952-01-01 to 1962-12-31, 
+--arrange in A-Z order by first Name
+--taking only the first 5 lines
+SELECT TOP 5 *
+FROM Employees
+WHERE LastName LIKE '%E%' AND BirthDate BETWEEN '1952-01-01' AND '1962-12-31'
+ORDER BY FirstName
+
+
+-- Challenge 2: Write an SQL statement to list employees fullname with 'U__' (two underscores) in the [Country]
+-- and start working from 1992-03-23 to 1994-12-31
+-- agrange in Z-A order by the last name
+SELECT *
+FROM Employees
+WHERE Country LIKE 'U__' AND HireDate BETWEEN '1992-03-23' AND '1994-12-31'
+ORDER BY LastName DESC
+
+
+
+
+
+
+
+
+
