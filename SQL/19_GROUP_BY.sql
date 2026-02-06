f5b81207068107c602c69bd59e11471919e337dc
Kiên Lê <100947959+trungkien2543@users.noreply.github.com>
2026-02-06 16:46:03 +0700

    feat: Bài 19,20

---
+++ SQL/19_GROUP_BY.sql
@@ -0,0 +1,64 @@
+﻿-- HÃY CHO BIẾT MỖI KHÁCH HÀNG ĐÃ ĐẶT BAO NHIÊU ĐƠN HÀNG
+SELECT CustomerID, COUNT(*) AS "SỐ ĐƠN ĐÃ ĐẶT"
+FROM Orders
+GROUP BY CustomerID
+ORDER BY [SỐ ĐƠN ĐÃ ĐẶT] DESC
+
+-- HÃY TÍNH GIÁ TRỊ ĐƠN GIÁ TRUNG BÌNH THEO MỖI NHÀ CUNG CẤP SẢN PHẨM
+SELECT SupplierID, AVG(UnitPrice) AS "GIÁ TRỊ TRUNG BÌNH"
+FROM Products
+GROUP BY SupplierID
+
+-- HÃY CHO BIẾT MỖI THỂ LOẠI CÓ TỔNG SỐ BAO NHIÊU SẢN PHẨM TRONG KHO
+SELECT CategoryID, SUM(UnitsInStock) AS "SỐ SẢN PHẨM MỖI LOẠI"
+FROM Products
+GROUP BY CategoryID
+
+-- HÃY CHO BIẾT GIÁ VẬN CHUYỂN THẤP NHẤT VÀ LỚN NHẤT CỦA CÁC ĐƠN HÀNG THEO TỪNG THÀNH PHỐ VÀ QUỐC GIA KHÁC NHAU
+SELECT ShipCountry, ShipCity, MIN(Freight) AS "GIÁ THẤP NHẤT", MAX(Freight) AS "GIÁ CAO NHẤT"
+FROM Orders
+GROUP BY ShipCountry, ShipCity
+ORDER BY ShipCountry, ShipCity
+
+
+-- BÀI TẬP 
+-- HÃY THỐNG KÊ SỐ LƯỢNG NHÂN VIÊN THEO TỪNG QUỐC GIA KHÁC NHAU
+SELECT Country, COUNT(*) AS "SỐ NHÂN VIÊN"
+FROM Employees
+GROUP BY Country
+
+--Thống kê số khách hàng ở từng quốc gia, từng thành phố
+--Tìm các khách hàng có CompanyName có chữ a trong tên
+--Sắp xếp theo chiều A-Z Country
+
+SELECT Country, City, COUNT(*) AS "SỐ KHÁCH HÀNG"
+FROM Customers
+WHERE CompanyName LIKE '%A%'
+GROUP BY Country, City
+ORDER BY Country
+
+
+-- Tính tổng số tiền vận chuyển ở từng thành phố, quốc gia
+-- Chỉ lấy những ShipName có có chữ 'b' 
+-- Sắp xếp theo chiều từ A-Z của ShipCountry
+
+-- ORDER BY CHỈ ĐƯỢC BAO GỒM CỘT CÓ TRONG GROUP BY
+
+SELECT ShipCountry, ShipCity, SUM(Freight) AS "TỔNG SỐ TIỀN VẬN CHUYỂN"
+FROM Orders
+WHERE ShipName LIKE '%B%'
+GROUP BY ShipCountry, ShipCity
+ORDER BY ShipCountry
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ SQL/20_DATE_FUNCTION.sql
@@ -0,0 +1,60 @@
+﻿-- TÍNH SỐ LƯỢNG ĐƠN ĐẶT HÀNG TRONG NĂM 1997 CỦA TỪNG KHÁCH HÀNG
+SELECT CustomerID, COUNT(*) AS "SỐ LƯỢNG ĐƠN ĐẶT HÀNG"
+FROM Orders
+WHERE YEAR(OrderDate) = 1997
+GROUP BY CustomerID
+
+
+-- LỌC RA CÁC ĐƠN HÀNG ĐƯỢC ĐẶT HÀNG VÀO THÁNG 5 NĂM 1997
+SELECT *
+FROM Orders
+WHERE YEAR(OrderDate) = 1997 AND MONTH(OrderDate) = 5
+
+-- LẤY DANH SÁCH CÁC ĐƠN HÀNG ĐƯỢC ĐẶT VÀO NGÀY 4 THÁNG 9 NĂM 1996
+SELECT *
+FROM Orders
+WHERE OrderDate = '1996-09-04'
+
+-- LẤY RA DANH SÁCH KHÁCH HÀNG ĐẶT HÀNG TRONG NĂM 1998 VÀ SỐ ĐƠN HÀNG MỖI THÁNG SẮP XẾP THEO THÁNG TĂNG DẦN
+SELECT  CustomerID, MONTH(OrderDate) AS "THÁNG", COUNT(*) AS "SỐ ĐƠN HÀNG"
+FROM Orders
+WHERE YEAR(OrderDate) = 1998
+GROUP BY CustomerID, MONTH(OrderDate)
+ORDER BY  MONTH(OrderDate)
+
+-- BÀI TẬP
+-- HÃY LỌC CÁC ĐƠN ĐẶT HÀNG ĐÃ ĐƯỢC GIAO VÀO THÁNG 5, VÀ SẮP XẾP TĂNG DẦN THEO NĂM
+SELECT *
+FROM Orders
+WHERE MONTH(ShippedDate) = 5
+ORDER BY YEAR(ShippedDate)
+
+
+-- Đếm tổng số đơn hàng và tổng số tiền vận chuyển (freight) CỦA TỪNG QUỐC GIA
+-- theo từng quốc gia trong bảng Orders, với đk:
+-- giao trễ (ShippedDate > RequiredDate)
+-- có ngày đặt hàng trong năm 1996 đến năm 1997
+-- sắp xếp lại A -> Z của tên nước
+
+SELECT ShipCountry, COUNT(*) AS "TỔNG SỐ ĐƠN", SUM(Freight) AS "TỔNG SỐ TIỀN VẬN CHUYỂN"
+FROM Orders
+WHERE ShippedDate > RequiredDate AND YEAR(OrderDate) IN (1996,1997)
+GROUP BY ShipCountry
+ORDER BY ShipCountry
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
