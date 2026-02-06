0fe1bf72205ba2728216e37bb5bf4a7693765732
Kiên Lê <100947959+trungkien2543@users.noreply.github.com>
2026-02-06 10:51:08 +0700

    feat: bài 16,17,18

---
+++ SQL/16_WILDCARD.sql
@@ -0,0 +1,39 @@
+﻿-- %	-> ĐẠI DIỆN CHO 0 HOẶC NHIỀU KÝ TỰ
+-- _	-> ĐẠI DIỆN CHO MỘT KÝ TỰ
+-- []	-> ĐẠI DIỆN CHO BẤT KỲ KÝ TỰ ĐƠN LẺ NÀO TRONG NGOẶC -> VÍ DỤ: h[oa]t thì tìm hot và hat, nhưng ko có hit
+-- ^	-> ĐẠI DIỆN CHO BẤT KỲ KÝ TỰ NÀO KHÔNG CÓ TRONG NGOẶC -> VÍ DỤ: h[^oa]t tìm hit, nhưng ko có hot và hat
+-- -	-> ĐẠI DIỆN CHO BẤT KỲ KÝ TỰ ĐƠN LẺ NÀO CÓ TRONG KHOẢNG NHẤT ĐỊNH -> VÍ DỤ: c[a-b]t tìm cat và cbt
+
+-- LỌC RA TẤT CẢ CÁC KHÁCH HÀNG CÓ TÊN LIÊN HỆ BẮT ĐẦU BẰNG CHỮ 'A'
+SELECT *
+FROM Customers
+WHERE ContactName LIKE 'A%'
+
+-- LỌC RA TẤT CẢ CÁC KHÁCH HÀNG CÓ TÊN LIÊN HỆ BẮT ĐẦU BẰNG CHỮ 'H', VÀ CÓ CHỮ THỨ 2 LÀ BẤT KỲ KÝ TỰ NÀO
+SELECT *
+FROM Customers
+WHERE ContactName LIKE 'H_%'
+
+-- HÃY LỌC RA TẤT CẢ CÁC ĐƠN HÀNG ĐƯỢC GỬI ĐẾN THÀNH PHỐ CÓ CHỮ CÁI BẮT ĐẦU LÀ L, CHỮ CÁI THỨ HAI LÀ U HOẶC O
+SELECT OrderID, ShipCity
+FROM Orders
+WHERE ShipCity LIKE 'L[U,O]%'
+
+-- HÃY LỌC RA TẤT CẢ CÁC ĐƠN HÀNG ĐƯỢC GỬI ĐẾN THÀNH PHỐ CÓ CHỮ CÁI BẮT ĐẦU LÀ L, CHỮ CÁI THỨ HAI KHÔNG PHẢI LÀ U HOẶC O
+SELECT OrderID, ShipCity
+FROM Orders
+WHERE ShipCity LIKE 'L[^U,O]%'
+
+
+-- LỌC RA TẤT CẢ CÁC ĐƠN HÀNG ĐƯỢC GỬI ĐẾN THÀNH PHỐ CÓ CHỮ BẮT ĐẦU LÀ L, CHỮ CÁI THỨ HAI LÀ CÁC KÝ TỰ TỪ A ĐẾN E
+SELECT OrderID, ShipCity
+FROM Orders
+WHERE ShipCity LIKE 'L[A-Z]%'
+
+-- BÀI TẬP
+-- HÃY LẤY RA TẤT CẢ CÁC NHÀ CUNG CẤP HÀNG CÓ TÊN CÔNG TY BẮT ĐẦU BẰNG CHỮ A VÀ KHÔNG CHỨA KÝ TỰ b
+-- LƯU Ý: [^B] CHỈ CÓ THỂ ĐẠI DIỆN CHO 1 KÝ TỰ KHÔNG THỂ DÙNG '%[^B]%' -> VÌ NÓ SẼ TÌM CHUỖI CÓ ÍT NHẤT 1 KÝ TỰ KHÁC B THÔI
+SELECT *
+FROM Suppliers
+WHERE NOT CompanyName LIKE '%B%' AND CompanyName LIKE 'A%'
+
---
+++ SQL/17_IN_NOT IN.sql
@@ -0,0 +1,52 @@
+﻿-- HÃY LỌC RA TẤT CẢ CÁC ĐƠN HÀNG VỚI ĐIỀU KIỆN:
+-- A) ĐƠN HÀNG ĐƯỢC GIAO ĐẾN TỪ GERMANY, UK, BRAZIL
+SELECT OrderID, ShipCountry
+FROM Orders
+WHERE ShipCountry IN ('GERMANY','UK','BRAZIL')
+ORDER BY ShipCountry
+
+-- B) ĐƠN HÀNG ĐƯỢC GIAO ĐẾN CÁC QUỐC GIA KHÁC GERMANY, UK, BRAZIL
+SELECT OrderID, ShipCountry
+FROM Orders
+WHERE ShipCountry NOT IN ('GERMANY','UK','BRAZIL')
+ORDER BY ShipCountry
+
+
+-- LẤY RA CÁC SẢN PHẨM CÓ MÃ THỂ LOẠI KHÁC VỚI 2, 3 VÀ 4
+SELECT ProductID, CategoryID
+FROM Products
+WHERE CategoryID NOT IN (2,3,4)
+ORDER BY CategoryID
+
+
+
+-- VÍ DỤ 3
+-- 1. HÃY LIỆT KÊ CÁC NHÂN VIÊN KHÔNG PHẢI NỮ TỪ BẢNG NHÂN VIÊN
+SELECT *
+FROM Employees
+WHERE TitleOfCourtesy IN ('Mr.','Dr.')
+
+-- 2. HÃY LIỆT KÊ CÁC NHÂN VIÊN LÀ NỮ TỪ BẢNG NHÂN VIÊN
+SELECT *
+FROM Employees
+WHERE TitleOfCourtesy NOT IN ('Mr.','Dr.')
+
+
+-- BÀI TẬP
+-- LẤY RA TẤT CẢ CÁC KHÁCH HÀNG ĐẾN TỪ CÁC THÀNH PHỐ SAU ĐÂY: BERLIN, LONDON, WARSZAWA
+SELECT *
+FROM Customers
+WHERE City IN ('BERLIN', 'LONDON', 'WARSZAWA')
+
+
+-- Hãy liệt kê tất cả các đơn hàng được gửi đến khách hàng trong ngày 1/1/1997 hoặc 2/1/1997
+SELECT *
+FROM Orders
+WHERE ShippedDate IN ('1997-01-01', '1997-01-02')
+
+
+
+
+
+
+
