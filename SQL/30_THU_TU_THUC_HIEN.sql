5b8f672d7cd44d26ad829dd9ddf8731f751485ff
Kiên Lê <100947959+trungkien2543@users.noreply.github.com>
2026-02-13 10:36:58 +0700

    feat: bài 30, 31

---
+++ SQL/30_THU_TU_THUC_HIEN.sql
@@ -0,0 +1,24 @@
+﻿-- 1. FROM
+-- 2. WHERE
+-- 3. GROUP BY
+-- 4. HAVING
+-- 5. SELECT
+-- 6. ORDER BY
+-- 7. LIMIT
+
+--SELECT DISTINCT 9 TOP 11, , 8
+--FROM 1
+--FULL/L/R 4 JOIN 2 ON 3
+--WHERE 5
+--GROUP BY 6
+--HAVING 7
+--ORDER BY 10
+
+select top 3 p.ProductID, o.Discount, o2.ShipCountry --> B7 :chọn cột và dòng cần lấy --> B9: lấy ra top 3
+from [dbo].[Products] p --> B1: kiểm tra bảng Products
+inner join [dbo].[Order Details] o --> B2: kiểm tra bảng Order Details
+on p.ProductID = o.ProductID --> B3: kiểm tra điều kiện từ 2 bảng Products và Order Details
+inner join [dbo].[Orders] o2 --> B4: kiểm tra bảng Orders
+on o.OrderID = o2.OrderID --> B5: kiểm tra điều kiện từ 2 bảng Orders và Order Details
+where o.Discount = 0.25 and o2.ShipCountry = 'Germany' --> B6: kiểm tra các điều kiện WHERE
+order by p.ProductID desc --> B8: sắp xếp dữ liệu theo p.ProductID
---
+++ SQL/31_COMMON_TABLE_EXPRESSION.sql
@@ -0,0 +1,152 @@
+﻿-- COMMON TABLE EXPRESSION (CTE) ĐƯỢC SỬ DỤNG ĐỂ TẠO BẢNG TẠM THỜI VÀ SỬ DỤNG NÓ TRONG CÁC TRUY VẤN SAU ĐÓ
+-- GIÚP LÀM CHO CÂU TRUY VẤN TRỞ NÊN DỄ ĐỌC VÀ DỄ QUẢN LÝ HƠN
+
+-- WITH TÊN_CTE (CỘT 1, CỘT 2, ...) AS (
+--			TRUY VẤN ĐỂ ĐỊNH NGHĨA CTE
+--)
+
+-- SAU CTE CHỈ TỒN TẠI 1 SELECT ĐƯỢC DÙNG CTE ĐÃ TẠO
+
+WITH SHORT_E AS (
+	SELECT EmployeeID, LastName, FirstName
+	FROM Employees
+)
+
+SELECT * FROM SHORT_E
+
+-- VÍ DỤ 1: LẤY THÔNG TIN VỀ CÁC SẢN PHẨM CÓ CÙNG THỂ LOẠI VỚI 1 SẢN PHẨM CỤ THỂ
+-- CÁCH 1: SUB QUERY
+SELECT *
+FROM Products
+WHERE CategoryID = (
+	SELECT CategoryID
+	FROM Products
+	WHERE ProductName = 'CHAI'
+)
+
+-- CÁCH 2: CTE
+WITH PRODUCT_CATEGORY AS (
+	SELECT CategoryID
+	FROM Products
+	WHERE ProductName = 'CHAI'
+)
+
+SELECT *
+FROM Products AS P
+JOIN PRODUCT_CATEGORY AS PC
+ON P.CategoryID = PC.CategoryID
+
+-- VÍ DỤ: LẤY THÔNG TIN ĐƠN HÀNG CÙNG VỚI TỔNG GIÁ TRỊ ĐƠN HÀNG VÀ TỶ LỆ GIỮA TỔNG GIÁ TRỊ VÀ PHÍ GIAO HÀNG
+-- CÁCH 1: CTE
+WITH TONG_GIA_TRI AS (
+	SELECT D.OrderID, SUM(D.Quantity*D.UnitPrice) AS 'TONG GIA TRI'
+	FROM [Order Details] AS D
+	GROUP BY D.OrderID
+), OTX AS (
+	SELECT D.OrderID, SUM(D.Quantity*D.UnitPrice) AS 'TONG GIA TRI'
+	FROM [Order Details] AS D
+	GROUP BY D.OrderID
+), OTX2 AS (
+	SELECT D.OrderID, SUM(D.Quantity*D.UnitPrice) AS 'TONG GIA TRI'
+	FROM [Order Details] AS D
+	GROUP BY D.OrderID
+)
+
+SELECT O.OrderID, T.[TONG GIA TRI], O.Freight, T.[TONG GIA TRI] / O.Freight AS 'TY LE GIUA TONG GIA TRI VA PHI GIAO HANG'
+FROM Orders AS O
+JOIN TONG_GIA_TRI AS T
+ON O.OrderID = T.OrderID
+
+
+-- CÁCH 2: SUB QUERY
+SELECT O.OrderID, T.[TONG GIA TRI], O.Freight, T.[TONG GIA TRI] / O.Freight AS 'TY LE GIUA TONG GIA TRI VA PHI GIAO HANG'
+FROM Orders AS O
+JOIN (
+	SELECT D.OrderID, SUM(D.Quantity*D.UnitPrice) AS 'TONG GIA TRI'
+	FROM [Order Details] AS D
+	GROUP BY D.OrderID
+) AS T
+ON O.OrderID = T.OrderID
+
+-- BÀI TẬP 
+-- 1. SỬ DỤNG CTE ĐỂ TÍNH TỔNG DOANH SỐ BÁN HÀNG CHO TỪNG SẢN PHẨM TỪ HAI BẢNG ORDER DETAILS VÀ PRODUCTS TRONG CƠ SỞ DỮ LIỆU NORTHWIND
+WITH DOANH_SO AS (
+	SELECT ProductID, SUM(Quantity*UnitPrice) AS 'DOANH SO'
+	FROM [Order Details] AS D
+	GROUP BY D.ProductID
+)
+
+SELECT P.ProductID, P.ProductName, D.[DOANH SO] 
+FROM Products AS P
+JOIN DOANH_SO AS D
+ON D.ProductID = P.ProductID
+
+-- 2. DÙNG CTE ĐỂ TÍNH TOÁN TỔNG DOANH SỐ BÁN HÀNG THEO TỪNG KHÁCH HÀNG VÀ SAU ĐÓ SẮP XẾP DANH SÁCH KHÁCH HÀNG THEO TỔNG DOANH SỐ GIẢM DẦN
+WITH GIA_TRI_DON_HANG AS (
+	SELECT OrderID, SUM(Quantity * UnitPrice) AS 'TONG GIA TRI'
+	FROM [Order Details]
+	GROUP BY OrderID
+)
+SELECT C.CustomerID, SUM(G.[TONG GIA TRI]) AS 'TONG DOANH SO'
+FROM Customers AS C
+LEFT JOIN Orders AS O
+ON C.CustomerID = O.CustomerID
+LEFT JOIN GIA_TRI_DON_HANG AS G
+ON G.OrderID = O.OrderID
+GROUP BY C.CustomerID
+ORDER BY [TONG DOANH SO] DESC
+
+-- KHÔNG DÙNG CTE
+SELECT C.CustomerID,
+       SUM(D.Quantity * D.UnitPrice) AS [TONG DOANH SO]
+FROM Customers C
+LEFT JOIN Orders O
+       ON C.CustomerID = O.CustomerID
+LEFT JOIN [Order Details] D
+       ON D.OrderID = O.OrderID
+GROUP BY C.CustomerID
+ORDER BY [TONG DOANH SO] DESC;
+
+
+-- 3. SỬ DỤNG CTE TÍNH TỔNG DOANH SỐ BÁN HÀNG THEO NĂM TỪ BẢNG ORDERS VÀ ORDER DETAILS
+WITH GIA_TRI_DON_HANG AS (
+	SELECT OrderID, SUM(Quantity * UnitPrice) AS 'TONG GIA TRI'
+	FROM [Order Details]
+	GROUP BY OrderID
+)
+SELECT YEAR(O.OrderDate) AS 'NAM', SUM(G.[TONG GIA TRI]) AS 'DOANH SO THEO NAM'
+FROM Orders AS O
+JOIN GIA_TRI_DON_HANG AS G
+ON O.OrderID = G.OrderID
+GROUP BY YEAR(O.OrderDate)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
