2519a731681ba25b9dbedfddfc4a8a0b956e2973
Le Trung Kien <trungkien1862@gmail.com>
2026-05-09 10:39:41 +0700

    Connect Database

--- Spring/study/src/main/java/com/springmasterclass/study/StudyApplication.java
+++ Spring/study/src/main/java/com/springmasterclass/study/StudyApplication.java
@@ -1,8 +1,14 @@
 package com.springmasterclass.study;
 
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
+
+@Log4j2
 @SpringBootApplication
 public class StudyApplication {
 
@@ -10,7 +16,10 @@ public class StudyApplication {
 		SpringApplication.run(StudyApplication.class, args);
 	}
 
-	// Starter: La cac goi Combo gom san thu vien
-	// Auto-Configuration: La co che tu dong thiet lap Bean dua tren nhung thu vien co san trong du an
-	// @Conditional: La bo dieu kien de Spring quyet dinh xem co nen tu cau hinh hay khong
+	@Bean
+	ApplicationRunner applicationRunner(Environment environment) {
+		return args -> {
+			log.info("message from application.properties " + environment.getProperty("POSTGRES_USER"));
+		};
+	}
 }
--- Spring/study/src/main/java/com/springmasterclass/study/entity/User.java
+++ Spring/study/src/main/java/com/springmasterclass/study/entity/User.java
@@ -1,6 +1,14 @@
 package com.springmasterclass.study.entity;
 
+import jakarta.persistence.*;
 
+@Entity
+@Table(name="tl_user")
 public class User {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.UUID)
+    private String id;
+    private String name;
+    private String username;
+    private String password;
 }
